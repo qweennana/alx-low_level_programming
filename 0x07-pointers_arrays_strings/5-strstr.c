289334a9a2e32449a6e15e23d2a89e2a1d7f8dd4
qweennana <125859109+qweennana@users.noreply.github.com>
2023-04-03 19:41:24 +0300

    TASK 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,7 +5,7 @@
  * _strstr - finds the first occurrence of the substring
  * @needle: input value
  * @haystack: input value
- * Return: a pointer to the beginning of the located substring
+ * Return: a pointer to the start of the located substring
  */
 
 char *_strstr(char *haystack, char *needle)
